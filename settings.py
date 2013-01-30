b562aa7c15bd9d2acad44129ef098b3eca1de008
Nick Saika <nicksaika@gmail.com>
2013-01-29 19:24:30 -0500

    Updated the settings.py file.

---
+++ settings.py
@@ -0,0 +1,20 @@
+# Bourbon's settings module.
+# 
+# To see a complete list of options that are supported by Flask (and in-turn,
+# Bourbon), see the following:
+#
+#    http://flask.pocoo.org/docs/config/#builtin-configuration-values
+
+# Set the hostname/IP address and port that Flask/Bourbon should listen on.
+# For production, un-comment this line
+#SERVER_NAME = "my.domain.com:8008"
+
+# Enable debugging?
+# 
+# If you set DEBUG = True, then Flask will automatically reload every time you
+# change a file. So, if you are developing Bourbon, it is usually a good idea
+# to set this to True.
+DEBUG = False
+
+# Enable/disable testing mode.
+TESTING = False
