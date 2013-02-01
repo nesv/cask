1482d8a7a08bdee266e5d742af939a231e10652c
Nick Saika <nicksaika@gmail.com>
2013-01-31 23:15:56 -0500

    Added a default route for "/"

--- bourbon/__init__.py
+++ bourbon/__init__.py
@@ -1,28 +1,26 @@
 """
 Bourbon is a Flask-powered content management system.
 """
-from blueprints.page import page
-from flask import Flask
+from flask import Flask, render_template
 import os
 import sys
-
-
-app = Flask(__name__.split(".")[0])
-app.register_blueprint(page)
-
-
 try:
     from flask.ext.sqlalchemy import SQLAlchemy
 except ImportError:
     print "You need to install the Flask-SQLAlchemy extension."
     sys.exit(os.EX_SOFTWARE)
 
+app = Flask(__name__.split(".")[0])
 db = SQLAlchemy(app)
+
+from blueprints.page import page
+app.register_blueprint(page, url_prefix="/pages")
+
+from blueprints.user import user
+app.register_blueprint(user)
+
 
 @app.route("/")
 def home():
-    """The index page for Bourbon."""
-    return "Grab your flask of bourbon, and have at it!"
-
-
-
+    """Loads the index page."""
+    return render_template("index.html")
